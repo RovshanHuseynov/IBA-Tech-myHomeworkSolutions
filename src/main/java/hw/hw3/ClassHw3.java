64849ecd80d2738f8bc6fe559e038ef447906a8c
PC <rovsen.huseyn.oglu@gmail.com>
2019-10-21 19:38:31 +0400

    -- first. Hw3. task finished

--- src/main/java/hw/hw3/ClassHw3.java
+++ src/main/java/hw/hw3/ClassHw3.java
@@ -1,4 +1,52 @@
 package hw.hw3;
 
+import java.util.Scanner;
+
 public class ClassHw3 {
+    static String [][] schedule = new String [7][2];
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input;
+        schedule[0][0] = "Sunday";
+        schedule[0][1] = "do home work";
+        schedule[1][0] = "Monday";
+        schedule[1][1] = "go to courses; watch a film";
+        schedule[2][0] = "Tuesday";
+        schedule[2][1] = "go to hometown";
+        schedule[3][0] = "Wednesday";
+        schedule[3][1] = "meet up with friends";
+        schedule[4][0] = "Thursday";
+        schedule[4][1] = "go shopping";
+        schedule[5][0] = "Friday";
+        schedule[5][1] = "do a course";
+        schedule[6][0] = "Saturday";
+        schedule[6][1] = "have a rest";
+
+        while(true) {
+            System.out.print("Please, input the day of the week: ");
+            input = in.nextLine();
+            if(input.equals("exit")){
+                break;
+            }
+
+            task(input);
+        }
+    }
+
+    public static void task(String s){
+        if(s.charAt(s.length()-1) == ' '){
+            s = s.substring(0,(s.length()-1));
+            // take into account that the user may have accidentally
+            // entered a space after the day of the week.
+        }
+        for(int i=0; i<7; i++) {
+            if (s.toLowerCase().equals(schedule[i][0].toLowerCase())){
+                System.out.println("Your tasks for " + schedule[i][0] + ": " + schedule[i][1]);
+                return;
+            }
+        }
+
+        System.out.println("Sorry, I don't understand you, please try again.");
+    }
 }
