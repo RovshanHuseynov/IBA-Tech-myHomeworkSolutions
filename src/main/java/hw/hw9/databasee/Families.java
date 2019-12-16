aff8513d6506f538b15051260cf45850731a425e
PC <rovsen.huseyn.oglu@gmail.com>
2019-12-16 18:30:27 +0400

    -- Hw9. update database

--- src/main/java/hw/hw9/databasee/Families.java
+++ src/main/java/hw/hw9/databasee/Families.java
@@ -8,9 +8,12 @@ import java.util.List;
 public class Families {
     Humans humans = new Humans();
     Family family1 = new Family(1, humans.family1Mother, humans.family1Father);
+    Family family2 = new Family(2, humans.family2Mother, humans.family2Father);
+    Family family3 = new Family(3, humans.family3Mother, humans.family3Father);
+    Family family4 = new Family(4, humans.family3Mother, humans.family3Father);
 
     public List<Family> create(){
-        List<Family> families = Arrays.asList(family1);
+        List<Family> families = Arrays.asList(family1, family2, family3, family4);
         return families;
     }
 }
--- src/main/java/hw/hw9/databasee/Humans.java
+++ src/main/java/hw/hw9/databasee/Humans.java
@@ -1,9 +1,9 @@
 package hw.hw9.databasee;
 
+import hw.hw9.entity.Family;
 import hw.hw9.entity.Human;
 import hw.hw9.enumm.DayOfWeek;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -45,9 +45,22 @@ public class Humans {
                 put(DayOfWeek.SUNDAY, Arrays.asList("have a rest"));
             }});
 
+    Human family2Mother = new Human("Ayise", "Agayeva", 1950);
+    Human family2Father = new Human("Samir", "Agayev", 1945);
+    Human family2Child1 = new Human("Amil", "Agayev", 1964);
+    Human family2Child2 = new Human("Selim", "Agayev", 1970);
+    Human family3Mother = new Human("Lale", "Melikova", 1950);
+    Human family3Father = new Human("Zamiq", "Melikov", 1945);
+    Human family3Child1 = new Human("Suqra", "Melikova", 1964);
+    Human family3Child2 = new Human("Nergiz", "Melikova", 1970);
+    Human family4Child1 = new Human("Emil", "Eliyev", 2001);
+
+
     public List<Human> create() {
         List<Human> humans = Arrays.asList(family1Mother, family1Father, family1Child1, family1Child2,
-                family1Child3, family1Child4, family1Child5, family1Child6, family1Child7, family1Child8);
+                family1Child3, family1Child4, family1Child5, family1Child6, family1Child7, family1Child8,
+                family2Mother, family2Father, family2Child1, family2Child2, family3Mother, family3Father,
+                family3Child1, family3Child2, family4Child1);
         return humans;
     }
 }
